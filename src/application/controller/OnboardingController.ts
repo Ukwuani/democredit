09c55273fb98497b1bfcc709b294ddc85ac555bb
ukwuani <ukwuanibarnabas@gmail.com>
2023-01-10 16:56:43 +0100

    DMC 10 (chore) add customer login controller

--- src/application/controller/OnboardingController.ts
+++ src/application/controller/OnboardingController.ts
@@ -3,17 +3,34 @@
 import { Router } from 'express';
 import {Customer} from "domain/customer/aggregators/Customer"
 import { CustomerService } from 'src/application/services/CustomerService';
-import { CustomerServiceImpl } from 'src/application/services/implementation/CustomerServiceImpl';
 import { run } from 'application/config/Async';
-import { ApiError } from 'application/config/Exception';
+import { APIError, BadRequest } from 'application/config/Exception';
 import { APIContext, Context } from '../config/APIWrapper';
 import { SignUpPayLoad } from '../payloads/SignUpPayload';
+import { APIErrorType } from '../config/Exception';
+import { CustomerServiceImpl } from 'src/application/services/implementation/CustomerServiceImpl';
+import {CustomerRepository} from "infrastructure/repository/CustomerRepository"
+import { OnboardingValidator } from 'application/validators/OnboardingValidator';
+import { LoginPayLoad } from '../payloads/LoginPayload';
+
+//could be injected with a lib
+const customerRepository = new CustomerRepository()
+const onboardingValidator = new OnboardingValidator()
+const customerService: CustomerService = new CustomerServiceImpl(customerRepository, onboardingValidator)
 
 
-const customerService: CustomerService = new CustomerServiceImpl()
+
 export class OnboardingController{
 
-  static async createCustomer(ctx: Context): Promise<Customer>{
-    return customerService.createCustomer(ctx.params as SignUpPayLoad, "234")
+  static async createCustomer(ctx: Context): Promise<Customer> {
+      return await customerService.createCustomer(ctx.params as SignUpPayLoad, "NGN")
+  }
+
+  static async listCustomer(ctx: Context){
+    return await customerService.getAllCustomers()
+  }
+
+  static async login(ctx: Context){
+    return await customerService.login(ctx.params as LoginPayLoad)
   }
 }
