ba27222f039398c464bdb3523b7fae9c991de12a
ukwuani <ukwuanibarnabas@gmail.com>
2023-01-10 17:09:22 +0100

    DMC 13 (chore) expose wallet routes

--- src/infrastructure/server/index.ts
+++ src/infrastructure/server/index.ts
@@ -1,16 +1,20 @@
 import dotenv from "dotenv";
 import express, { ErrorRequestHandler , Express} from "express";
 import { createServer as createHTTPServer, Server } from 'http';
+import {router as OnboardingRoutes} from "src/infrastructure/routes/OnboardingRoutes";
+import {router as WalletRoutes} from "src/infrastructure/routes/WalletRoutes";
+
+
 
 dotenv.config()
 const app: Express = express();
 const server: Server = createHTTPServer(app);
-import {router as OnboardingRoutes} from "application/routes/OnboardingRoutes";
 
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
 app.use("/api/v1/customer", OnboardingRoutes);
+app.use("/api/v1/wallet", WalletRoutes);
 
 app.use("/",(req, res, next) => {res.send("DemoCredit")});
 
