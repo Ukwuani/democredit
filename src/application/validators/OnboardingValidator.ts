2555440bc10f56fdbfbe7654c2de599a8428f243
ukwuani <ukwuanibarnabas@gmail.com>
2023-01-10 16:58:26 +0100

    DMC 10 (chore) add customer payload validator

---
+++ src/application/validators/OnboardingValidator.ts
@@ -0,0 +1,24 @@
+import Validator from "fastest-validator"
+import {SignUpPayLoad} from "application/payloads/SignUpPayload"
+import { LoginPayLoad } from 'application/payloads/LoginPayload';
+export class OnboardingValidator extends Validator {
+    async validateSignUpPayload(data: SignUpPayLoad) {
+        let schema: SignUpPayLoad = {
+            firstName: "string|min:2",
+            lastName: "string|min:2",
+            phoneNumber: "string|min:10",
+            email: "email",
+            password: "string|min:6"
+        }
+        return await this.compile(schema)(data)
+    }
+
+    async validateloginPayload(data: LoginPayLoad) {
+        let schema: LoginPayLoad = {
+            loginId: ["email", "string|min:10"],
+            password: "string|min:6"
+        }
+        return await this.compile(schema)(data)
+    }
+
+}
